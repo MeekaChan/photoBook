9711054e3af1d3dfe3d34c396c0607fbe1cc0310
MeekaChan <emhannibal@gmail.com>
2015-04-30 14:51:31 +0200

    Improve background options

--- Script.js
+++ Script.js
@@ -6,32 +6,32 @@
 
 $(document).ready(function() {
     $("#table").click(function() {
-	    $("html").css("background-image", "url(http://mayang.com/textures/Wood/images/Flat%20Wood%20Textures/wooden_tabletop_1012121.JPG)").css("background-size", "cover");
+	    $("html").css("background-image", "url(Images/Background/wooden_tabletop_1012121.JPG)").css("background-size", "cover");
     });
 
     $("#concrete").click(function() {
-        $("html").css("background-image", "url(http://www.precisionskateboard.com/blog/wp-content/uploads/2013/06/wildtextures-concrete-wall-background.jpg)").css("background-size", "cover");
+        $("html").css("background-image", "url(Images/Background/wildtextures-concrete-wall-background.jpg)").css("background-size", "cover");
     });
 
-    $("#planks").click(function() {
+    /* $("#planks").click(function() {
         $("html").css("background-image", "url(http://benshabtay.com/3qchina1/Background1.jpg)").css("background-size", "cover");
-    });
+    }); */
 
-    $("#wooden").click(function() {
+    /* $("#wooden").click(function() {
         $("html").css("background-image", "url(http://congregationalehouse.com/wp-content/uploads/2014/04/wooden-table-background-brqkvusf.jpg)").css("background-size", "cover");
-    });
+    }); */
 
     $("#grass").click(function() {
-        $("html").css("background-image", "url(https://lawnstarter.com/blog/wp-content/uploads/2015/01/Yellow-st-augustine-lawn.jpg)").css("background-size", "cover");
+        $("html").css("background-image", "url(Images/Background/depositphotos_3258821-Grass-background---golf-field.jpg)").css("background-size", "cover");
     });
 
-    $("#pergament").click(function() {
+    /* $("#pergament").click(function() {
         $("html").css("background-image", "url(http://www.morocco-luxury-camps.com/wp-content/uploads/2012/11/paper-background.jpg)").css("background-size", "cover");
-    });
+    }); */
 
-    $("#paper").click(function() {
+    /* $("#paper").click(function() {
         $("html").css("background-image", "url(http://www.publicdomainpictures.net/pictures/20000/velka/rough-beige-paper-texture.jpg)").css("background-size", "cover");
-    });
+    }); */
 
     $("#blank").click(function() {
         $("html").css("background", "#FFCDAA", "background-image", "url(none)").css("background-size", "cover");
